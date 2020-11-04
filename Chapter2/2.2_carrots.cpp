c7f208deec115ad72e470b7d04f2e40d19e3f0ba
tianchao <tianchao@pku.edu.cn>
2020-11-04 16:14:08 +0800

    update chapter2

---
+++ Chapter2/2.2_carrots.cpp
@@ -0,0 +1,18 @@
+//carrots.cpp -- food processing program
+
+#include <iostream>
+
+int main() {
+    using namespace std;
+    int carrots;
+    carrots = 25;
+    
+    cout << "I have ";
+    cout << carrots;
+    cout << " carrots.";
+    cout << endl;
+
+    carrots = carrots - 1;
+    cout << "Crunch, crunch. Now I have " << carrots << " carrots." << endl;
+    return 0;
+}
